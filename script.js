4d99143abd1caa7f6ba6d940a01e426fad6f1135
Mitesh Rege <miteshrege4@gmail.com>
2023-08-09 21:13:48 +0530

    added removeitem & clearitem functionalities

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ItemForm = document.getElementById("item-form");
 const ItemInput = document.getElementById("item-input");
 const Itemlist = document.getElementById("item-list");
-
+const clearbtn = document.getElementById("clear")
 const additem = (e) =>{
     e.preventDefault();
     // Validating the input
@@ -16,9 +16,13 @@ const additem = (e) =>{
 
    const button = createButton("remove-item btn-link text-red");
    li.appendChild(button);
+    //   Adding  New Item using DOM
    Itemlist.appendChild(li)
    ItemInput.value = ""
+   updateUI();
 }
+
+
 function createButton(classes){
     const button = document.createElement("button");
     button.className=classes;
@@ -26,16 +30,46 @@ function createButton(classes){
     button.appendChild(icon);
     return button;
 };
+
+
 function createIcon(classes){
     const icon = document.createElement("i");
     icon.className=classes;
     return icon;
 };
 
+const removeitem = e =>{
+    
+    if (e.target.parentElement.classList.contains("remove-item")){
+        console.log(e.target.parentElement.parentElement.remove());
+    }
+    updateUI();
+};
+const clearitem = e =>{
+    while (Itemlist.firstChild){
+        // Itemlist.innerHTML='';
+        Itemlist.removeChild(Itemlist.firstChild)
+    }
+    updateUI();
+};
 
+const updateUI=()=>{
+    const items = Itemlist.querySelectorAll("li");
+    if (items.length === 0){
+        clearbtn.style.display = 'none';
+    } 
+    else{
+        clearbtn.style.display = 'block';
+    }
+};
+
+//  Event Listerners
 ItemForm.addEventListener("submit",additem)
+Itemlist.addEventListener("click",removeitem)
+clearbtn.addEventListener("click",clearitem)
 
 
+updateUI();
 
 
 
